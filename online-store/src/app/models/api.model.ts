15403c23c23cfe523a9bbee0893c4f25805984b6
Vlad <vladdaking@gmail.com>
2020-09-29 13:53:03 +0300

    Tried to add documentation comments :)

--- online-store/src/app/models/api.model.ts
+++ online-store/src/app/models/api.model.ts
@@ -1,14 +1,16 @@
 export type EndpointMethod = 'GET' | 'POST' | 'UPDATE';
 
+/* Endpoint metadata. Contains relative path and request type */
 export interface EndpointMetadata {
   path: string;
   method: EndpointMethod;
 }
-
+/* Known endpoints enumeration */
 interface KnownEndpoints {
   getFiltered: EndpointMetadata;
 }
 
+/* Endpoints interface. Each of the fields describes endpoint method metadata */
 export interface Endpoints extends KnownEndpoints{
   [key: string]: EndpointMetadata;
 }
