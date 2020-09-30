eb2af13f1b85b80ebf52352676382c9932405844
Vlad <vladdaking@gmail.com>
2020-09-30 21:54:35 +0300

    Add font mixins

--- online-store/src/app/modules/page-layout/page-layout.module.ts
+++ online-store/src/app/modules/page-layout/page-layout.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { PageLayoutComponent } from './page-layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -9,6 +11,8 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [PageLayoutComponent, HeaderComponent, FooterComponent],
   imports: [
     CommonModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [
     PageLayoutComponent,
