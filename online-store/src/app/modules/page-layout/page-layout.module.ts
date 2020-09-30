a3961b4fd2dfadfadae7d7870b188f0598646f85
Vlad <vladdaking@gmail.com>
2020-09-30 23:18:58 +0300

    Logo now is a link

--- online-store/src/app/modules/page-layout/page-layout.module.ts
+++ online-store/src/app/modules/page-layout/page-layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 import { PageLayoutComponent } from './page-layout.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
@@ -13,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
     CommonModule,
     MatIconModule,
     MatButtonModule,
+    RouterModule,
   ],
   exports: [
     PageLayoutComponent,
